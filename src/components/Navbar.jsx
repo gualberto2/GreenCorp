333f9de84144491b735c35534bc6219c48e6cc4b
Arthur Fernandez <arthur@snooget.com>
2024-01-17 19:19:27 -0800

    feat: navbar style pt:1
    - first iteration of navbar missing some alignment and thickness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,18 +2,20 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <div className="bg-slate-950/70 p-8 top-0 left-0 absolute w-full">
+    <div className="bg-slate-950/70 top-0 left-0 absolute w-full">
       {/* The parent div of the entire component ABOVE */}
       <div className="flex justify-around text-white">
         {/* The PARENT div of the navigation bar is ABOVE*/}
-        <h1>KNJ Green Corp</h1>
-        <ul className="flex space-x-20">
-          <li>Home</li>
-          <li>About</li>
-          <li>Products</li>
-          <li>Services</li>
-          <li>
-            <div>Contact</div>
+        <h1 className="text-lg">KNJ Green Corp</h1>
+        <ul className="flex space-x-20 text-md">
+          <li className="hover:cursor-pointer">Home</li>
+          <li className="hover:cursor-pointer">About</li>
+          <li className="hover:cursor-pointer">Products</li>
+          <li className="hover:cursor-pointer">Services</li>
+          <li className="hover:cursor-pointer">
+            <button className="border-1 border-blue-400 bg-green-500 p-2 rounded-xl">
+              Contact
+            </button>
           </li>
         </ul>
       </div>
