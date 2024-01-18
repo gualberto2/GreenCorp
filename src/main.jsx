f3d5e5a10091190af18512ba90c4b02bd4dbdf8e
Arthur Fernandez <arthur@snooget.com>
2024-01-18 10:31:19 -0800

    feat: Route Defined
    - Fix undefined route

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar.jsx";
 
 // import App from "./App.jsx";
 import "./index.css";
-//
+
+// Page imports
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
