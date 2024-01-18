4134cfdbf25343e6c031d804342bfbe29d928149
Arthur Fernandez <arthur@snooget.com>
2024-01-18 07:45:51 -0800

    feat: Router Dom install
    - Now can switch between pages in the website.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 // import { useState } from "react";
 import "./App.css";
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
 
 function App() {
   // const [count, setCount] = useState(0);
 
   return (
     <>
-      <Home />
-      {/* Below here will be the hero-section component */}
+      {/* <Home /> */}
+      {/*  */}
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// import App from "./App.jsx";
+import "./index.css";
+//
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
