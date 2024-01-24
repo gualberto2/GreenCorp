45f9b9492b9737cdfc05971cae8df58f5783277b
gualberto2 <elijahwilsonnn@gmail.com>
2024-01-23 18:40:45 -0800

    idk tbh

--- src/main.jsx
+++ src/main.jsx
@@ -18,7 +18,7 @@ export default function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="about" element={<About />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </Router>
     </>
