36d3c57b12fcf8cdb347e1ba7340f2963d9533e8
gualberto2 <elijahwilsonnn@gmail.com>
2024-01-24 11:16:10 -0800

    fixed merge issues

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,7 @@
 import React, { useState } from "react";
 
-
 import { IoMdArrowDropright } from "react-icons/io";
 import { FaArrowRightToBracket } from "react-icons/fa6";
-
-export default function Home() {
-
 
 // Card Imports:
 import Card from "../components/Card";
@@ -20,7 +16,7 @@ import { FaRecycle } from "react-icons/fa";
 export default function Home() {
   // Below is state for the about us / services card section
   const [activeItem, setActiveItem] = useState("Roving Fabric");
-  
+
   const [hoveredCard, setHoveredCard] = useState(1);
 
   const products = [
@@ -78,7 +74,6 @@ export default function Home() {
     return (hoveredCard - 1) * 100;
   };
 
-  
   const renderItemContent = () => {
     switch (activeItem) {
       case "Roving Fabric":
@@ -96,6 +91,9 @@ export default function Home() {
               className="max-w-full h-auto"
               src="https://kmsibir.ru/upload/iblock/a85/a85a874075cd91c71c1677d06466be53.jpg"
             />
+          </div>
+        );
+
       case "Fiberglass Products":
         return (
           <div className="flex flex-col md:flex-row">
@@ -184,8 +182,6 @@ export default function Home() {
             />
           </div>
         );
-      default:
-        return null;
     }
   };
 
@@ -225,13 +221,6 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div>
-      <div className="hero-bg h-screen flex items-center justify-center">
-        <div className="grid grid-cols-4 grid-rows-4 gap-12 text-white ">
-          <div className="border-2 border-slat-400 row-start-3 row-end-5 flex flex-col items-center justify-center p-6 relative shadow-black shadow-xl">
-            <span className="plus-sign left font-thin">+</span>
-            <p className="font-thin text-4xl ">200 Tons</p>
-            <p className="pt-6 text-lg">Recycled plastic per year</p>
 
       {/* Product section */}
       <div className="">
@@ -292,63 +281,63 @@ export default function Home() {
           </div>
         </div>
 
-      {/* About & Services */}
-      <div className="grid grid-cols-12 grid-rows-1">
-        <div className="col-start-1 col-end-7 row-start-0">
-          {/* Conditional Images will go HERE!! */}
-          {hoveredImage && (
-            <div className="h-[100%] overflow-clip">
-              {/* Adjust height as needed */}
-              <img
-                src={hoveredImage.img}
-                alt={`Service ${hoveredCard}`}
-                className="w-full h-full object-cover"
+        {/* About & Services */}
+        <div className="grid grid-cols-12 grid-rows-1">
+          <div className="col-start-1 col-end-7 row-start-0">
+            {/* Conditional Images will go HERE!! */}
+            {hoveredImage && (
+              <div className="h-[100%] overflow-clip">
+                {/* Adjust height as needed */}
+                <img
+                  src={hoveredImage.img}
+                  alt={`Service ${hoveredCard}`}
+                  className="w-full h-full object-cover"
+                />
+              </div>
+            )}
+          </div>
+          <div className="col-start-7 col-end-13 justify-center">
+            <div className="flex gap-2 hover:cursor-pointer mt-10 mx-2">
+              <Card
+                onMouseEnter={() => setHoveredCard(1)}
+                onMouseLeave={() => setHoveredCard(1)}
+                icon={<FaRecycle />}
+                title="Recycling"
+                description="Together we reduce carbon footprints by collecting your recycable materials"
+              />
+              <Card
+                onMouseEnter={() => setHoveredCard(2)}
+                onMouseLeave={() => setHoveredCard(1)}
+                icon={<AiFillCustomerService />}
+                title="Quality"
+                description="Our team of 150 hand sorters ensure high quality products"
+              />
+              <Card
+                onMouseEnter={() => setHoveredCard(3)}
+                onMouseLeave={() => setHoveredCard(1)}
+                icon={<GoLaw />}
+                title="Mix"
+                description="Increase ESG Scores with 80% Virgin & 20% Recycled Products"
               />
             </div>
-          )}
-        </div>
-        <div className="col-start-7 col-end-13 justify-center">
-          <div className="flex gap-2 hover:cursor-pointer mt-10 mx-2">
-            <Card
-              onMouseEnter={() => setHoveredCard(1)}
-              onMouseLeave={() => setHoveredCard(1)}
-              icon={<FaRecycle />}
-              title="Recycling"
-              description="Together we reduce carbon footprints by collecting your recycable materials"
-            />
-            <Card
-              onMouseEnter={() => setHoveredCard(2)}
-              onMouseLeave={() => setHoveredCard(1)}
-              icon={<AiFillCustomerService />}
-              title="Quality"
-              description="Our team of 150 hand sorters ensure high quality products"
-            />
-            <Card
-              onMouseEnter={() => setHoveredCard(3)}
-              onMouseLeave={() => setHoveredCard(1)}
-              icon={<GoLaw />}
-              title="Mix"
-              description="Increase ESG Scores with 80% Virgin & 20% Recycled Products"
-            />
+          </div>
+          <div className="sliding-bar-container col-start-7 col-end-13">
+            <div
+              className="sliding-bar pt-2"
+              style={{
+                transform: `translateX(${calculateTranslateX(hoveredCard)}%)`,
+              }}
+            ></div>
           </div>
         </div>
-        <div className="sliding-bar-container col-start-7 col-end-13">
-          <div
-            className="sliding-bar pt-2"
-            style={{
-              transform: `translateX(${calculateTranslateX(hoveredCard)}%)`,
-            }}
-          ></div>
+        {/* Products */}
+        <div className="text-center bg-slate-950 p-[10rem] text-amber-100">
+          <div>
+            <h1 className="text-4xl font-bold">Products</h1>
+          </div>
+          {/* Products cards below */}
+          <ProductSlider products={products} />
         </div>
-      </div>
-      {/* Products */}
-      <div className="text-center bg-slate-950 p-[10rem] text-amber-100">
-        <div>
-          <h1 className="text-4xl font-bold">Products</h1>
-        </div>
-        {/* Products cards below */}
-        <ProductSlider products={products} />
-
       </div>
     </div>
   );
