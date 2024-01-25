27ce69f4ddf468be1e99268a287c4c1a8ab5927e
Arthur Fernandez <arthur@snooget.com>
2024-01-25 15:41:43 -0800

    feat: Mobile footer
    Create mobile footer with tailwindcss, and react

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,60 +1,116 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 // import KNJLogo from "../path-to-your-logo/logo.svg"; // Update with your logo path
 
 const Footer = () => {
   return (
-    <div className="bg-gray-800 text-white">
-      <div className="container mx-auto py-10 px-4">
-        <div className="flex md:flex-row justify-between">
-          {/* Footer Menu */}
-          <div className="mb-6 md:mb-0">
-            <ul className="flex justify-evenly">
-              <li className="mx-2">
-                <a href="/about-us" className="hover:text-gray-300">
-                  About Us
-                </a>
-              </li>
-              <li className="mx-2">
-                <a href="/services" className="hover:text-gray-300">
-                  Services
-                </a>
-              </li>
-              <li className="mx-2">
-                <a href="/contact" className="hover:text-gray-300">
-                  Contact
-                </a>
-              </li>
-              <li className="mx-2">
-                <a href="/privacy-policy" className="hover:text-gray-300">
-                  Privacy Policy
-                </a>
-              </li>
-              {/* Add additional links as needed */}
-            </ul>
-          </div>
-
-          {/* Contact Information */}
-          <div className="footer-contacts text-center md:text-right mt-6 md:mt-0">
-            <div className="footer-title mb-2 font-bold">Contact Us</div>
+    <footer>
+      <div>
+        <div>
+          <div>
             <div>
-              <a href="tel:+1234567890" className="block hover:text-gray-300">
-                +1 234 567 890
-              </a>
-              <a href="mailto:contact@knj.com" className="hover:text-gray-300">
-                contact@knj.com
-              </a>
+              <form onSubmit="">
+                <div className="w-[70%] mx-auto">
+                  {/* This div above houses all the div with sib labels, inputs, buttons for the form */}
+                  <div className="">
+                    {/* This div is parent of fname lname parent divs */}
+                    <div className="flex flex-col">
+                      <label
+                        htmlFor="first"
+                        className="font-light text-md text-gray"
+                      >
+                        First Name
+                      </label>
+                      <input
+                        type="text"
+                        id="first"
+                        placeholder="ex. John"
+                        value=""
+                        onChange=""
+                        className="text-center w-full px-3 py-2 focus:ring-transparent bg-gray-200 border-gray-200 focus:bg-gray-300 focus:border-blue-800 border-[1px] rounded-md "
+                        required
+                      />
+                    </div>
+                    <div>
+                      <label
+                        htmlFor="last"
+                        className="font-light text-md text-gray"
+                      >
+                        Last Name
+                      </label>
+                      <input
+                        type="text"
+                        id="last"
+                        placeholder="ex. Wick"
+                        value=""
+                        onChange=""
+                        className="text-center w-full px-3 py-2 focus:ring-transparent bg-gray-200 border-gray-200 focus:bg-gray-300 focus:border-blue-800 border-[1px] rounded-md "
+                        required
+                      />
+                    </div>
+                  </div>
+                  <div className="flex flex-col">
+                    <label
+                      htmlFor="email"
+                      className="font-light text-md text-gray"
+                    >
+                      Email
+                    </label>
+                    <input
+                      type="email"
+                      id="email"
+                      value=""
+                      placeholder="johnwick@email.com"
+                      onChange=""
+                      className="text-center transition duration-150 ease-in-out  py-3 rounded-md bg-gray-200 border-gray-200 border-[1px] focus:bg-gray-300  focus:border-blue-800 focus:ring-transparent w-full"
+                      required
+                    />
+                  </div>
+                  <div className="flex flex-col">
+                    <label
+                      htmlFor="phone"
+                      className="font-light text-md text-gray"
+                    >
+                      Phone Number
+                    </label>
+                    <input
+                      type="tel"
+                      id="phone"
+                      value=""
+                      onChange=""
+                      placeholder="(123)-456-7890"
+                      className="text-center transition duration-150 ease-in-out  py-3 rounded-md bg-gray-200 border-gray-200 border-[1px] focus:bg-gray-300  focus:border-blue-800 focus:ring-transparent w-full"
+                      required
+                    />
+                  </div>
+                  <div className="pt-4">
+                    <button
+                      type="submit"
+                      className="w-full py-3 px-4 font-light text-md text-white bg-[#566E3D] rounded-md"
+                    >
+                      Submit
+                    </button>
+                  </div>
+                </div>
+              </form>
             </div>
-            <div className="mt-4">
-              <address className="not-italic">
-                123 KNJ Street, City, Country
-                <br />
-                Mon-Fri 09:00 - 18:00
-              </address>
+          </div>
+          <div className="flex flex-col w-[70%] mx-auto text-center pt-4">
+            <div className="pt-4 border-t border-slate-400"></div>
+            <div className="flex justify-between text-lg">
+              <div className="lg:hover:cursor-pointer lg:hover:underline lg:decoration-green-500">
+                <p>Home</p>
+              </div>
+              <div className="lg:hover:cursor-pointer lg:hover:underline lg:decoration-green-500">
+                <p>About</p>
+              </div>
+              <div className="lg:hover:cursor-pointer lg:hover:underline lg:decoration-green-500">
+                <p>Contact</p>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </footer>
   );
 };
 
