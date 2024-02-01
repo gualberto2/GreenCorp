7e22e10ba79f626f8b1433d6ad055eb1357bf2a0
Arthur Fernandez <arthur@snooget.com>
2024-02-01 10:30:41 -0800

    feat: finished footer design
    - Simple minimalist footer responsive for mobile and desktop users

--- src/components/GlobalComponents/Footer.jsx
+++ src/components/GlobalComponents/Footer.jsx
@@ -3,61 +3,65 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="lg:text-left pt-20 flex items-center">
+    <footer className="bg-gray-200 mt-2 pt-2 lg:mt-10 md:mt-10">
       {/* Navigation Links */}
-      <div className="mx-auto text-center">
-        <div className="font-light pb-10">
-          <div className="flex flex-col text-left lg:text-lg lg:pr-4 gap-4">
-            <p className="lg:hover:cursor-pointer lg:hover:underline lg:decoration-green-500 hidden lg:block md:block">
-              Home
-            </p>
-            <p className="lg:hover:cursor-pointer lg:hover:underline lg:decoration-green-500">
-              About
-            </p>
-            <p className="lg:hover:cursor-pointer lg:hover:underline lg:decoration-green-500">
-              Products
-            </p>
-            <p className="lg:hover:cursor-pointer lg:hover:underline lg:decoration-green-500">
-              Contact
-            </p>
-          </div>
+      <div>
+        <div className="flex gap-4">
+          <p className="hover:cursor-pointer hover:underline decoration-green-500">
+            Home
+          </p>
+          <p className="hover:cursor-pointer hover:underline decoration-green-500">
+            About
+          </p>
+          <p className="hover:cursor-pointer hover:underline decoration-green-500">
+            Products
+          </p>
+          <p className="hover:cursor-pointer hover:underline decoration-green-500">
+            Contact
+          </p>
         </div>
+        <div className="mt-2  mb-2 border-b border-black"></div>
       </div>
       <div>
-        <h2 className="text-center text-2xl font-semibold">Inquiries?</h2>
-        <form onSubmit="" className="w-[80%] mx-auto">
-          <div className="flex flex-col items-center">
-            <div className="lg:flex">
-              <div className="lg:w-1/2 lg:pr-2">
-                <label htmlFor="first" className="font-light text-md text-gray">
-                  Name
-                </label>
-                <input
-                  type="text"
-                  id="first"
-                  placeholder="ex. John Wick"
-                  className="text-center w-full px-3 py-2 focus:ring-transparent bg-gray-200 border-gray-200 focus:bg-gray-300 focus:border-blue-800 border-[1px] rounded-md"
-                  required
-                />
-              </div>
+        <p className="text-sm font-light">
+          KnJ Green Pack Corp™
+          <span className="pl-2 text-xs font-extralight">
+            Powered by <span>Ocomni</span>
+          </span>
+        </p>
+      </div>
+
+      {/* Div for the footer form */}
+      {/* <div className="bg-slate-100 lg:border-none md:border-none border-black mt-8 w-full"> */}
+      {/* <form onSubmit="" className="">
+          <div className="flex flex-col lg:flex-row md:flex-row items-center gap-2 lg:items-end md:items-end">
+            <div className="lg:w-1/2 md:w-1/2 lg:pr-2 md:pr-2">
+              <label htmlFor="first" className="font-light text-md text-gray">
+                Name
+              </label>
+              <input
+                type="text"
+                id="first"
+                placeholder="ex. John Wick"
+                className="text-center w-full px-3 py-2 focus:ring-transparent bg-gray-200 border-gray-200 focus:bg-gray-300 focus:border-blue-800 border-[1px] rounded-md"
+                required
+              />
             </div>
 
-            <div className="lg:flex">
-              <div className="lg:w-1/2 lg:pr-2">
-                <label htmlFor="email" className="font-light text-md text-gray">
-                  Email
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  placeholder="johnwick@email.com"
-                  className="text-center w-full px-3 py-2 focus:ring-transparent bg-gray-200 border-gray-200 focus:bg-gray-300 focus:border-blue-800 border-[1px] rounded-md"
-                  required
-                />
-              </div>
+            <div className="lg:w-1/2 md:w-1/2 lg:pr-2 md:pr-2">
+              <label htmlFor="email" className="font-light text-md text-gray">
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                placeholder="johnwick@email.com"
+                className="text-center w-full px-3 py-2 focus:ring-transparent bg-gray-200 border-gray-200 focus:bg-gray-300 focus:border-blue-800 border-[1px] rounded-md"
+                required
+              />
             </div>
 
-            <div className="pt-4 lg:w-full text-left">
+            <div className="text-center">
               <button
                 type="submit"
                 className="py-3 px-4 font-light text-md text-white bg-[#59c9a5] rounded-md"
@@ -66,8 +70,8 @@ const Footer = () => {
               </button>
             </div>
           </div>
-        </form>
-      </div>
+        </form> */}
+      {/* </div> */}
     </footer>
   );
 };
