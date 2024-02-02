dd53288e918de33f33fdbbd1373bb1843d022e52
Arthur Fernandez <arthur@snooget.com>
2024-02-02 13:56:23 -0800

    feat: Contact page in mainjs
    - Add contact page to main.js file with <Route> from react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,7 @@ import "./index.css";
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
 import Products from "./pages/Products.jsx";
+import Contact from "./pages/Contact.jsx";
 import Footer from "./components/GlobalComponents/Footer.jsx";
 
 export default function App() {
@@ -22,6 +23,7 @@ export default function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/products" element={<Products />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
         <Footer />
       </Router>
